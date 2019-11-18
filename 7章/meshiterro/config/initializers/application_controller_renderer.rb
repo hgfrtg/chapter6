110eb333f5ab2ed8b9dab68fd7dc1d2ce310111a
hgfrtg <jrbevknf@outlook.com>
2019-11-18 14:33:24 +0900

    [Finish] 6-7

---
+++ 7章/meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c66a58f0da6511a03a7cb68ce4f8a41f554c94119eb8fa167faf71db0d48f93858d4cad2c2027bddf32996b5608c153a1d5ae1562f79d897927b6fb08556639b'
